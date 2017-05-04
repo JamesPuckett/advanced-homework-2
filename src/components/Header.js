c736e86e274d3374bfac0201ea7c49c886855224
James Puckett <jamesjaypuckett@gmail.com>
2017-05-04 12:47:18 -0500

    HW2 first commit, working on linking changeCat to top nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,26 @@
 import React from "react";
+import TopHeader from "./TopHeader";
+import SubHeader from "./SubHeader";
+import TopNav from "./TopNav";
 
-function Header() {
-  return (<div />);
+function Header(props) {
+  console.log('header props', props)
+  return (
+    <div className="header">
+
+      <TopHeader />  
+    
+      <div className="clear" />
+    
+      <SubHeader />
+    
+      <div className="clear" />
+      
+      <TopNav changeCat={props.changeCat} />
+    
+    </div>
+			
+  );
 }
 
 export default Header;
--- src/index.js
+++ src/index.js
@@ -5,8 +5,14 @@ import "./index.css";
 import state from "./state";
 
 function render() {
+  let currentCategory = state.products;
+  console.log("index current cat", currentCategory);
+  function changeCategory(category) {
+    currentCategory = category;
+    render(currentCategory);
+  }
   ReactDOM.render(
-    <App state={state} />,
+    <App state={state} cat={changeCategory} />,
     document.getElementById("root")
   );
 }
